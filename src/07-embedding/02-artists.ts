f6bb28bcbaabe28e0c6eb4d7b7e4a252470f148c
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-10 18:49:03 -0400

    feat(02): configure automatic vectorization with openai

--- src/07-embedding/02-artists.ts
+++ src/07-embedding/02-artists.ts
@@ -1,11 +1,65 @@
 import { ARTIST_IDS } from "./artist-ids"
+import weaviate from "weaviate-ts-client"
+
+const CLASS_NAME: string = "ArtistBio2"
 
 async function main() {
   const artists = await fetchArtists({ size: 100 })
-  console.log(artists)
+  await prepareArtistsCollection()
+  await insertArtists(artists)
 }
 
 main()
+
+async function prepareArtistsCollection() {
+  const client = weaviate.client({
+    scheme: "https",
+    host: "https://weaviate.stg.artsy.systems",
+  })
+
+  const alreadyExists = await client.schema.exists(CLASS_NAME)
+
+  if (alreadyExists) {
+    console.log(`${CLASS_NAME} class already exists, deleting it`)
+    await client.schema.classDeleter().withClassName(CLASS_NAME).do()
+  }
+
+  const classWithProps = {
+    class: CLASS_NAME,
+    vectorizer: "text2vec-openai",
+    moduleConfig: {
+      "text2vec-openai": {
+        model: "ada",
+        dimensions: 1536,
+        type: "text",
+        vectorizeClassName: false,
+      },
+    },
+    properties: [
+      {
+        name: "blurb",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+            vectorizePropertyName: false,
+          },
+        },
+      },
+    ],
+  }
+
+  const classResult = await client.schema
+    .classCreator()
+    .withClass(classWithProps)
+    .do()
+
+  console.log(JSON.stringify(classResult, null, 2))
+}
+
+async function insertArtists(artists: { slug: string; blurb: string }[]) {
+  console.log(`Going to insert ${artists.length} artists`)
+}
 
 async function fetchArtists(args: { size: number }) {
   const query = `query getArtists ($size: Int!, $ids: [String]!) {
