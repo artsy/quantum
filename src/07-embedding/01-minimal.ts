54c5c4d65abd53716c6a8074c20eea4f00a07fcf
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-13 12:24:25 -0400

    feat: add examples for embedding and semantic search (#8)
    * build: add support for weaviate
    
    * build: add lodash
    
    * feat(01): embed two artist bios
    
    * feat(01): persist the two bio embeddings
    
    * feat(01): embed a user "bio" and query with it
    
    * feat(02): fetch artist blurbs from MP
    
    * feat(02): configure automatic vectorization with openai
    
    * feat(02): insert and automatically vectorize artists
    
    * feat(02): fetch 1000 artists and insert them in batches of 100
    
    * feat(02): add link to query console

---
+++ src/07-embedding/01-minimal.ts
@@ -0,0 +1,133 @@
+import OpenAI from "openai"
+import dotenv from "dotenv"
+import weaviate from "weaviate-ts-client"
+import _ from "lodash"
+
+dotenv.config()
+const openai = new OpenAI()
+
+// spell-checker:disable
+const artist_1_bio = `Yayoi Kusama dazzles audiences worldwide with her immersive “Infinity Mirror Rooms” and an aesthetic that embraces light, polka dots, and pumpkins. The avant-garde artist first rose to prominence in 1960s New York, where she staged provocative Happenings and exhibited hallucinatory paintings of loops and dots that she called “Infinity Nets.” Kusama also influenced Andy Warhol and augured the rise of feminist and Pop art. She has been the subject of major exhibitions at the Museum of Modern Art, Centre Pompidou, Tate Modern, and the National Museum of Modern Art in Tokyo. In 1993, Kusama represented Japan at the Venice Biennale. Today, her work regularly sells for seven figures on the secondary market. Throughout her disparate practice, Kusama has continued to explore her own obsessive-compulsive disorder, sexuality, freedom, and perception. In 1977, Kusama voluntarily checked herself into a psychiatric hospital in Tokyo, where she continues to live today.`
+
+const artist_2_bio = `Claude Monet’s lush, light-dappled plein air paintings exemplify the aesthetics of the Impressionist movement, which the artist helped establish in late 1800s France. Monet keenly observed and rendered urban environments, his iconic water lily gardens, haystacks, and other pastoral landscapes. He painted each setting over and over again in order to capture changes in light and ambiance. As he evoked the particularities of the environment, his brushstrokes could veer towards abstraction. Both his mark-making and rich color palettes helped establish a path for 20th-century painting. Monet’s work belongs in the collections of the Metropolitan Museum of Art, the Musée de l’Orangerie, the Art Institute of Chicago, the Museum of Fine Arts, Boston, and many others. In 2019, his painting Mueles (1891) became the most expensive Impressionist artwork ever sold at auction when it notched $110.7 million.`
+// spell-checker:enable
+
+async function main() {
+  /*
+   * EMBED with OpenAI
+   */
+
+  const artist_1_response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: artist_1_bio,
+    encoding_format: "float",
+  })
+
+  const artist_1_vector = artist_1_response.data[0].embedding
+
+  const artist_2_response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: artist_2_bio,
+    encoding_format: "float",
+  })
+
+  const artist_2_vector = artist_2_response.data[0].embedding
+
+  console.log("artist_1_vector", artist_1_vector.slice(0, 10))
+  console.log("artist_2_vector", artist_2_vector.slice(0, 10))
+
+  /*
+   * PERSIST with Weaviate
+   */
+
+  const client = weaviate.client({
+    scheme: "https",
+    host: "https://weaviate.stg.artsy.systems",
+  })
+
+  // delete the class if it exists
+
+  const className: string = "ArtistBio" // Replace with your class name
+  await client.schema.classDeleter().withClassName(className).do()
+
+  // Add the class to the Weaviate schema
+
+  const classWithProps = {
+    class: "ArtistBio",
+
+    properties: [
+      {
+        name: "bio",
+        dataType: ["text"],
+      },
+    ],
+  }
+
+  const classResult = await client.schema
+    .classCreator()
+    .withClass(classWithProps)
+    .do()
+
+  console.log(classResult) // The returned value is the full class definition
+
+  // Insert the bios as new objects to the ArtistBio collection
+
+  let objectResult = await client.data
+    .creator()
+    .withClassName("ArtistBio")
+    .withProperties({
+      bio: artist_1_bio,
+    })
+    .withVector(artist_1_vector)
+    .do()
+
+  console.log(_.omit(objectResult, "vector")) // the returned value is the object
+
+  objectResult = await client.data
+    .creator()
+    .withClassName("ArtistBio")
+    .withProperties({
+      bio: artist_2_bio,
+    })
+    .withVector(artist_2_vector)
+    .do()
+
+  console.log(_.omit(objectResult, "vector")) // the returned value is the object
+
+  /*
+   * Embed a user bio and issue a query based on it
+   */
+
+  // Tweak this to try out different queries
+  const user_bio = `I am interested in optical illusions`
+
+  const user_response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: user_bio,
+    encoding_format: "float",
+  })
+
+  const user_vector = user_response.data[0].embedding
+
+  // Query the ArtistBio collection for records that are "near" the user bio
+
+  const search_result = await client.graphql
+    .get()
+    .withClassName("ArtistBio")
+    .withNearVector({ vector: user_vector })
+    .withLimit(1)
+    .withFields("bio _additional { distance }")
+    .do()
+
+  console.log(
+    "\nQuerying for artist bio nearest to this user bio:\n\n",
+    user_bio,
+    "\n"
+  )
+  console.log(JSON.stringify(search_result, null, 2))
+}
+
+main()
+
+// And now you can view the results of this in Weaviate's GraphQL playground:
+// https://link.weaviate.io/3QBEmlH
---
+++ src/07-embedding/02-artists.ts
@@ -0,0 +1,151 @@
+import { ARTIST_IDS } from "./artist-ids"
+import weaviate from "weaviate-ts-client"
+import _ from "lodash"
+
+const client = weaviate.client({
+  scheme: "https",
+  host: "https://weaviate.stg.artsy.systems",
+})
+
+const CLASS_NAME: string = "ArtistBio2"
+
+const BATCH_SIZE = 100
+
+async function main() {
+  const artists = await fetchArtists()
+  await prepareArtistsCollection()
+  await insertArtists(artists)
+  /*
+   * now query from Weaviate console:
+   * https://link.weaviate.io/3UEZ854
+   */
+}
+
+main()
+
+async function prepareArtistsCollection() {
+  const client = weaviate.client({
+    scheme: "https",
+    host: "https://weaviate.stg.artsy.systems",
+  })
+
+  const alreadyExists = await client.schema.exists(CLASS_NAME)
+
+  if (alreadyExists) {
+    console.log(`${CLASS_NAME} class already exists, deleting it`)
+    await client.schema.classDeleter().withClassName(CLASS_NAME).do()
+  }
+
+  const classWithProps = {
+    class: CLASS_NAME,
+    vectorizer: "text2vec-openai",
+    moduleConfig: {
+      "text2vec-openai": {
+        model: "ada",
+        dimensions: 1536,
+        type: "text",
+        vectorizeClassName: false,
+      },
+    },
+    properties: [
+      {
+        name: "blurb",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+            vectorizePropertyName: false,
+          },
+        },
+      },
+      {
+        name: "slug",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: true,
+          },
+        },
+      },
+    ],
+  }
+
+  const classResult = await client.schema
+    .classCreator()
+    .withClass(classWithProps)
+    .do()
+
+  console.log(JSON.stringify(classResult, null, 2))
+}
+
+async function insertArtists(artists: { slug: string; blurb: string }[]) {
+  console.log(`Going to insert ${artists.length} artists`)
+
+  const batches = _.chunk(artists, BATCH_SIZE)
+  console.log(`Inserting ${batches.length} batches`)
+
+  for (const artistBatch of batches) {
+    let batcher = client.batch.objectsBatcher()
+    batcher = batcher.withObjects(
+      ...artistBatch.map((artist) => ({
+        class: CLASS_NAME,
+        properties: artist,
+      }))
+    )
+    process.stdout.write(".")
+    await batcher.do()
+  }
+  process.stdout.write("\n")
+}
+
+async function fetchArtists() {
+  const query = `query getArtists ($size: Int!, $ids: [String]!) {
+    artistsConnection(first: $size, ids: $ids) {
+      edges {
+        node {
+          slug
+          blurb
+        }
+      }
+    }
+  }
+  `
+
+  const headers = {
+    "Content-Type": "application/json",
+  }
+
+  const batches = _.chunk(ARTIST_IDS, BATCH_SIZE)
+
+  const artists = await Promise.all(
+    batches.map(async (ids) => {
+      const variables = {
+        size: ids.length,
+        ids,
+      }
+
+      const response = await metaphysics({ query, variables, headers })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return response.data.artistsConnection.edges.map((edge: any) => edge.node)
+    })
+  )
+
+  return artists.flat()
+}
+
+async function metaphysics(args: {
+  query: string
+  variables: Record<string, unknown>
+  headers: Record<string, string>
+}) {
+  const { query, variables, headers } = args
+
+  const url = "https://metaphysics-staging.artsy.net/v2"
+
+  const body = JSON.stringify({ query, variables })
+  const options = { method: "POST", headers, body }
+
+  const response = await fetch(url, options)
+  const json = await response.json()
+  return json
+}
