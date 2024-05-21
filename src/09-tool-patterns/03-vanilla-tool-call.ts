61f5e654b327a09575301d8b86fe9f2bbb37a088
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-21 10:52:17 -0400

    feat(tools): use vercel tool with vendor api client

---
+++ src/09-tool-patterns/03-vanilla-tool-call.ts
@@ -0,0 +1,64 @@
+import dotenv from "dotenv"
+import OpenAI from "openai"
+import dedent from "dedent"
+import { zodToJsonSchema } from "zod-to-json-schema"
+import { fetchShowsNearLocation } from "../lib/tools"
+
+dotenv.config()
+const openai = new OpenAI()
+
+async function main() {
+  // build initial message list
+
+  const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+      role: "user",
+      content: dedent`
+        Give me 3 gallery shows happening in New York City"
+      `,
+    },
+  ]
+
+  console.log("Message:", messages)
+
+  // define tools, by accessing the AI SDK tool's attributes
+  // and shaping them for the vendor's API client
+
+  const tools: OpenAI.Chat.ChatCompletionTool[] = [
+    {
+      type: "function",
+      function: {
+        name: "fetchShowsNearLocation",
+        description: fetchShowsNearLocation.description,
+        parameters: zodToJsonSchema(fetchShowsNearLocation.parameters),
+      },
+    },
+  ]
+
+  // get response, via vendor's vanilla chat completion
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    temperature: 0,
+    tools,
+    messages,
+  })
+
+  // handle the tool call
+
+  const toolCall = response.choices[0].message.tool_calls![0]
+
+  console.log("\nTool call:", toolCall.function)
+
+  if (toolCall.function.name === "fetchShowsNearLocation") {
+    const args = JSON.parse(toolCall.function.arguments)
+    const result = await fetchShowsNearLocation.execute(args)
+
+    console.log("\nTool result:", result)
+    process.exit(0)
+  }
+
+  throw new Error(`Unexpected tool call: ${toolCall.function.name}`)
+}
+
+main()
