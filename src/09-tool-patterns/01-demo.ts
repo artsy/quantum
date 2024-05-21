72b5518187339fd2d85fb8b17b11cb1bf8d5bc59
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-21 10:52:10 -0400

    feat(demo): tool loop agent

--- src/09-tool-patterns/01-demo.ts
+++ src/09-tool-patterns/01-demo.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import chalk from "chalk"
-import { generateText, streamText, tool } from "ai"
+import { CoreMessage, generateText, streamText, tool } from "ai"
 import { openai } from "@ai-sdk/openai"
 import { anthropic } from "@ai-sdk/anthropic"
 import dedent from "dedent"
@@ -93,10 +93,82 @@ async function main() {
   console.log(chalk.bold.blue("\nResponse text"))
   console.log(toolResponse.toolCalls)
   console.log(toolResponse.toolResults)
+
+  /*
+   * Tool loop demo, kind of a baby agent
+   */
+
+  const messages: CoreMessage[] = [
+    {
+      role: "system",
+      content: dedent`
+        You are a terse math assistant. You use tools to answer the user's question
+      `,
+    },
+    {
+      role: "user",
+      content: dedent`
+        Which is greater, the average of the first 3 odd numbers, or the first 2 even numbers?
+      `,
+    },
+  ]
+
+  const MAX_ITERATIONS = 10
+  let i = 0
+  let isDone = false
+
+  console.log(chalk.bold.red("\nTool loop agent"))
+
+  // loop until done
+  while (!isDone && i < MAX_ITERATIONS) {
+    const response = await generateText({
+      model: openai("gpt-4o"),
+      temperature: 0,
+      messages,
+      tools: {
+        addTwoNumbers,
+        multiplyTwoNumbers,
+        subtractTwoNumbers,
+        divideTwoNumbers,
+      },
+    })
+
+    switch (response.finishReason) {
+      case "stop":
+        // we have generated a response and are done, so respond and quit
+        isDone = true
+        console.log(chalk.bold.blue(`\nResponse text (iteration #${++i})`))
+        console.log(chalk.bold.green(response.text))
+        console.log(chalk.bold.blue.dim(`\nIn response to original prompt:`))
+        console.log(chalk.dim(messages[1].content))
+        break
+
+      case "tool-calls":
+        // we have called tools, so add the tool calls & results to the message list
+        messages.push(
+          {
+            role: "assistant",
+            content: response.toolCalls,
+          },
+          {
+            role: "tool",
+            content: response.toolResults,
+          }
+        )
+        console.log(chalk.bold.blue(`\nTool call (iteration #${++i})`))
+        console.log(response.toolCalls)
+        console.log(response.toolResults)
+        break
+
+      default:
+        // there could be other reasons, but we ignore that for now
+        throw new Error(`Unhandled finish reason: ${response.finishReason}`)
+    }
+  }
 }
 
 /*
- * Sample tool
+ * Sample tool library
  */
 
 const addTwoNumbers = tool({
@@ -106,6 +178,33 @@ const addTwoNumbers = tool({
     b: z.number(),
   }),
   execute: async ({ a, b }) => Promise.resolve(a + b),
+})
+
+const multiplyTwoNumbers = tool({
+  description: "Multiply two numbers",
+  parameters: z.object({
+    a: z.number(),
+    b: z.number(),
+  }),
+  execute: async ({ a, b }) => Promise.resolve(a * b),
+})
+
+const subtractTwoNumbers = tool({
+  description: "Subtract two numbers",
+  parameters: z.object({
+    a: z.number(),
+    b: z.number(),
+  }),
+  execute: async ({ a, b }) => Promise.resolve(a - b),
+})
+
+const divideTwoNumbers = tool({
+  description: "Divide two numbers",
+  parameters: z.object({
+    a: z.number(),
+    b: z.number(),
+  }),
+  execute: async ({ a, b }) => Promise.resolve(a / b),
 })
 
 main()
