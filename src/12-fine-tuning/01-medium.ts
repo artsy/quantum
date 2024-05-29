8da0b4583e1a6ca88c9097a7a7ea69d985705540
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-29 17:59:38 -0400

    feat: upload prepared datasets for fine-tuning

--- src/12-fine-tuning/01-medium.ts
+++ src/12-fine-tuning/01-medium.ts
@@ -1,9 +1,11 @@
 import { prepareData } from "./lib/data"
+import { uploadData } from "./lib/file"
 
 export const SELECTED_INPUT = "medium-sample-100"
 
 async function main() {
   await prepareData()
+  await uploadData()
 }
 
 main()
