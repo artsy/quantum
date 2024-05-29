9ddbe9df43fabc284687a4295d9ea5bb0ea90050
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-29 18:45:28 -0400

    docs: add comment to script

--- src/12-fine-tuning/01-medium.ts
+++ src/12-fine-tuning/01-medium.ts
@@ -5,6 +5,16 @@ import { assessFineTune, summarizeResults } from "./lib/test"
 
 export const SELECTED_INPUT = "medium-sample-100"
 
+/**
+ * Adds a script to create a fine-tuned model for inferring artwork mediums from their materials descriptions
+ *
+ * yarn tsx src/12-fine-tuning/01-medium.ts
+ *
+ * This will generate properly formatted training data, upload it, and kick off a fine-tuning job.
+ *
+ * Once it completes, it will send it some test data and print out the results.
+ */
+
 async function main() {
   await prepareData()
   const uploadedFiles = await uploadData()
