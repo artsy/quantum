81167ed052a3786dfa9ed8f507e04d687769edc9
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-06-07 17:23:25 -0400

    feat: add a script to generate image descriptions for current artworks

--- src/14-curated-discovery/helpers.ts
+++ src/14-curated-discovery/helpers.ts
@@ -2,6 +2,9 @@ import { GravityArtwork } from "./types"
 import path from "path"
 import fs from "fs"
 import _ from "lodash"
+import { CoreMessage, generateObject } from "ai"
+import { z } from "zod"
+import dedent from "dedent"
 
 /**
  * Read artworks from a local JSON file
@@ -52,7 +55,7 @@ export function resizeImage(
   }
 ) {
   const { width, height } = _.defaults(options, { width: 512, height: 512 })
-  return `https://d7hftxdivxxvm.cloudfront.net/?src=${src}&resize_to=fit&width=${width}&height=${height}&grow=false`
+  return `https://d7hftxdivxxvm.cloudfront.net/?src=${src}&resize_to=fit&width=${width}&height=${height}&convert_to=jpg&grow=false`
 }
 
 /**
@@ -74,4 +77,83 @@ export async function getImageDataForArtworks(
   )
   const imageData = _.fromPairs(imageFieldPairs)
   return imageData
+}
+
+/**
+ * Use multimodal models’ computer vision capabilities to
+ * describe an artwork image.
+ *
+ * @param args.url URL of the artwork image that is to be described
+ * @param args.model Model to use, as defined by Vercel AI SDK
+ * @param args.maxWords Maximum number of words for prose description of the image (default: 200)
+ * @returns An object that describes the image
+ */
+export async function describeImage(args: {
+  url: string
+  model: Parameters<typeof generateObject>[0]["model"]
+  maxWords?: number
+}) {
+  const { url, model } = args
+  const maxWords = args.maxWords || 200
+
+  const VALID_COLORS = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "brown",
+    "gray",
+    "pink",
+  ]
+
+  const system = dedent`
+    You are an expert on art and your task is to provide detailed
+    descriptions of artworks.
+
+    Consider the visual qualities of the artwork, as well as
+    the artwork's relation to art historical styles and movements.
+
+    If you detect any objects in the image, include them in your output.
+
+    If you detect any words in the image, include them in your output.
+
+    Include up to 3 prominent colors in your output, excluding any background paper, canvas, or wall colors. The only valid colors are ${VALID_COLORS.join(", ")}.
+  `
+
+  const schema = z.object({
+    description: z.string(),
+    objects: z.string().array().optional(),
+    words: z.string().array().optional(),
+    colors: z.string().array().max(3),
+  })
+
+  const messages: CoreMessage[] = [
+    {
+      role: "user",
+      content: [
+        {
+          type: "text",
+          text: dedent`
+            Analyze the artwork depicted in this image.
+
+            Limit the description to ${maxWords} words or less.
+          `,
+        },
+        {
+          type: "image",
+          image: await getBase64Blob(url),
+        },
+      ],
+    },
+  ]
+
+  return await generateObject({
+    model,
+    temperature: 0,
+    system,
+    messages,
+    schema,
+  })
 }
