ad22f8f9ae7b86424b91b25e062a54fa792803ea
Matt Jones <matt.jones@artsymail.com>
2024-10-07 16:03:25 -0400

    feat: add infinite discovery users and artworks schemas

---
+++ src/17-infinite-discovery/02-artworks.ts
@@ -0,0 +1,126 @@
+import weaviate from "weaviate-ts-client"
+import dotenv from "dotenv"
+import { deleteIfExists } from "system/weaviate"
+import { ArtworksClassName } from "./types"
+
+dotenv.config()
+
+// Constants
+const CLASS_NAME: ArtworksClassName = "InfiniteDiscoveryArtworks"
+
+const client = weaviate.client({
+  host: process.env.WEAVIATE_URL!,
+})
+
+async function main() {
+  await prepareCollection()
+}
+
+main()
+
+async function prepareCollection() {
+  await deleteIfExists(CLASS_NAME)
+
+  const classWithProps = {
+    class: CLASS_NAME,
+    vectorizer: "text2vec-openai",
+    moduleConfig: {
+      "text2vec-openai": {
+        model: "text-embedding-3-small",
+        dimensions: 1536,
+        type: "text",
+        vectorizeClassName: false,
+      },
+    },
+    properties: [
+      {
+        name: "internalID",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+          },
+        },
+      },
+      {
+        name: "rarity",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: true,
+          },
+        },
+      },
+      {
+        name: "medium",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+          },
+        },
+      },
+      {
+        name: "saleMessage",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+          },
+        },
+      },
+      {
+        name: "colors",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+          },
+        },
+      },
+      {
+        name: "slug",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: true,
+          },
+        },
+      },
+      {
+        name: "url",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: true,
+          },
+        },
+      },
+      {
+        name: "title",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: false,
+          },
+        },
+      },
+      {
+        name: "imageUrl",
+        dataType: ["text"],
+        moduleConfig: {
+          "text2vec-openai": {
+            skip: true,
+          },
+        },
+      },
+    ],
+  }
+
+  const classResult = await client.schema
+    .classCreator()
+    .withClass(classWithProps)
+    .do()
+
+  console.log(JSON.stringify(classResult, null, 2))
+}
---
+++ src/17-infinite-discovery/types.ts
@@ -0,0 +1,5 @@
+export type ArtworksClassName = "InfiniteDiscoveryArtworks"
+
+export type UsersClassName = "InfiniteDiscoveryUsers"
+
+export type User = { internalID: string; name: string }
